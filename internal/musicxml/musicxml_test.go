a458fd6ae300ba31663b8d6c62f5d292eb01d1fe
David Kleiven <davidkleiven446@gmail.com>
2025-02-19 21:15:56 +0100

    feat: add function to extract information

--- internal/musicxml/musicxml_test.go
+++ internal/musicxml/musicxml_test.go
@@ -8,20 +8,38 @@ import (
 	"testing"
 )
 
-func TestRead(t *testing.T) {
+func testScore() (Scorepartwise, error) {
 	_, currentFile, _, _ := runtime.Caller(0)
 	currentDir := filepath.Dir(currentFile)
 	testData := filepath.Join(currentDir, "../../test/data/testScore.musicxml")
 	data, err := os.ReadFile(testData)
+	var document Scorepartwise
+	if err != nil {
+		return document, err
+	}
+
+	err = xml.Unmarshal(data, &document)
+	return document, err
+}
+
+func TestRead(t *testing.T) {
+	document, err := testScore()
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	var document Scorepartwise
-	err = xml.Unmarshal(data, &document)
-	if err != nil {
-		t.Error(err)
-		return
+	fields := TextFields(document)
+	fieldMap := make(map[string]bool)
+	for _, v := range fields {
+		fieldMap[v] = true
+	}
+
+	expect := []string{"Unknown composer", "Test score", "Score subtitle", "Calmly", "Agitato", "A"}
+	for _, v := range expect {
+		if _, ok := fieldMap[v]; !ok {
+			t.Errorf("%s not in %v\n", v, fieldMap)
+			return
+		}
 	}
 }
---
+++ internal/musicxml/utils_test.go
@@ -0,0 +1,107 @@
+package musicxml
+
+import (
+	"testing"
+)
+
+func newMeasure(measureNo string, dirType *Directiontype) Measure {
+	return Measure{
+		Measureattributes: Measureattributes{NumberAttr: measureNo},
+		Musicdata:         Musicdata{Direction: []*Direction{{Directiontype: []*Directiontype{dirType}}}},
+	}
+}
+
+func TestDirectionFromMeasure(t *testing.T) {
+	formattedText := Formattedtextid{Value: "value"}
+	dirTypeRehersalNil := Directiontype{Rehearsal: []*Formattedtextid{nil}}
+	dirTypeWOrdNil := Directiontype{Words: []*Formattedtextid{nil}}
+
+	dirTypeRehersalFilled := Directiontype{Rehearsal: []*Formattedtextid{&formattedText}}
+	dirTypeWordFilled := Directiontype{Words: []*Formattedtextid{&formattedText}}
+
+	for _, test := range []struct {
+		measure Measure
+		want    []MeasureTextResult
+		desc    string
+	}{
+		{
+			Measure{Musicdata: Musicdata{Direction: []*Direction{nil}}},
+			[]MeasureTextResult{},
+			"Direction type nil",
+		},
+		{
+			newMeasure("1", &dirTypeRehersalNil),
+			[]MeasureTextResult{},
+			"Rehersal content nil",
+		},
+		{
+			newMeasure("1", &dirTypeWOrdNil),
+			[]MeasureTextResult{},
+			"Word content nil",
+		},
+		{
+			newMeasure("1", &dirTypeRehersalFilled),
+			[]MeasureTextResult{{Number: 1, Text: "value"}},
+			"Rehersal non nil",
+		},
+		{
+			newMeasure("1", &dirTypeWordFilled),
+			[]MeasureTextResult{{Number: 1, Text: "value"}},
+			"Word filled non nil",
+		},
+		{
+			newMeasure("non-int", &dirTypeWordFilled),
+			[]MeasureTextResult{},
+			"Word filled wrong measure number",
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+
+			result := DirectionFromMeasure(test.measure)
+
+			if len(result) != len(test.want) {
+				t.Errorf("Wanted %v got %v", test.want, result)
+			}
+
+			for i := range result {
+				if result[i].Number != test.want[i].Number || result[i].Text != test.want[i].Text {
+					t.Errorf("Wanted %v got %v", test.want, result)
+				}
+			}
+		})
+	}
+}
+
+func TestMeasureText(t *testing.T) {
+	value := Formattedtextid{Value: "value"}
+	dirType := Directiontype{Rehearsal: []*Formattedtextid{&value}}
+
+	m1 := newMeasure("1", &dirType)
+	m2 := newMeasure("2", &dirType)
+	measures := []*Measure{&m1, &m2}
+
+	want := []MeasureTextResult{
+		{
+			Number: 1,
+			Text:   "value",
+		},
+		{
+			Number: 2,
+			Text:   "value",
+		},
+	}
+
+	result := MeasureText(measures)
+
+	if len(result) != len(want) {
+		t.Errorf("Wanted %v got %v\n", result, want)
+		return
+	}
+
+	for i := range result {
+		if result[i].Number != want[i].Number || result[i].Text != want[i].Text {
+			t.Errorf("Wanted %v got %v\n", result[i], want[i])
+		}
+	}
+
+}
