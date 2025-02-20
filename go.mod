084f9af0db22db271ea5e0f4f2893176f4d3655e
David Kleiven <davidkleiven446@gmail.com>
2025-02-20 18:40:42 +0100

    feat(test): ensure xml round trip works

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/davidkleiven/silent-score
 
 go 1.23.3
+
+require (
+	github.com/beevik/etree v1.5.0
+	github.com/ucarion/c14n v0.1.0
+)
--- scripts/formatXml.sh
+++ scripts/formatXml.sh
@@ -1,15 +1,21 @@
 retval=0
 tmpfile=$(mktemp)
+tmpfile2=$(mktemp)
 echo "Using temp file $tmpfile"
 while read -r file; do
     echo "Proceccing file $file"
-    xmllint --format $file --output $tmpfile
-    diff $file $tmpfile > /dev/null
+    go run scripts/sortMusicxml/sort_musicxml.go $file $tmpfile
+    xmllint --dropdtd $tmpfile --output $tmpfile
+    xmllint --format $tmpfile --output $tmpfile
+    xmllint --exc-c14n $tmpfile > $tmpfile2
+    sed -i -e '$a\' $tmpfile2
+    diff $file $tmpfile2 > /dev/null
     if [ $? -ne 0 ]; then
         echo "$file was reformatted"
         retval=1
     fi
-    mv $tmpfile $file
+    mv $tmpfile2 $file
 done < <(find "." -type f \( -name "*.xml" -o -name "*.xsd" -o -name "*.musicxml"  \))
+rm $tmpfile
 echo "Exiting with status $retval"
 exit $retval
